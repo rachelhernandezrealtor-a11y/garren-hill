96acdc88603e2be3eca45874a925cacdbc0958d3
rachelhernandezrealtor-a11y <rachelhernandezrealtor@gmail.com>
2026-04-22 21:25:45 -0400

    Migrate to Vite: src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GarranHillV2 from './GarranHillV2.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GarranHillV2 />
+  </React.StrictMode>
+)
